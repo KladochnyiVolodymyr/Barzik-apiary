a7d6b950b6c537a8dcee7253790f27ebbdda5f81
Volodymyr Kladochnyi <kladochnyi.v@gmail.com>
2017-01-27 23:13:40 +0200

    added the anchor in the menu

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function Anchor(active){
+            if(active==true){
+                var hash=window.location.hash;
+                if(hash){
+                    $(".nav-list a").removeClass("active");
+                    $("a[href="+hash+"]").addClass("active");
+                }
+            }
+            function AnchorHref(){
+                var anchor=$(this);
+                if(hash){
+                    $(".nav-list a").removeClass("active");
+                    anchor.addClass("active");
+                }
+                $("html, body").animate({
+                    scrollTop: $(anchor.attr("href")).offset().top
+                }, 1000);
+            }
+            $("a[href*=#]").click(AnchorHref);
+        }
+        $(Anchor(true));
