60f742412a68a17f7863a2e3033f762e72958b6d
Volodymyr Kladochnyi <kladochnyi.v@gmail.com>
2017-01-28 23:12:01 +0200

    Changes in menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
-function Anchor(active){
-            if(active==true){
-                var hash=window.location.hash;
-                if(hash){
-                    $(".nav-list a").removeClass("active");
-                    $("a[href="+hash+"]").addClass("active");
-                }
-            }
-            function AnchorHref(){
-                var anchor=$(this);
-                if(hash){
-                    $(".nav-list a").removeClass("active");
-                    anchor.addClass("active");
-                }
-                $("html, body").animate({
-                    scrollTop: $(anchor.attr("href")).offset().top
-                }, 1000);
-            }
-            $("a[href*=#]").click(AnchorHref);
-        }
-        $(Anchor(true));
+$(document).ready(function(){
+    $(".nav-list").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
